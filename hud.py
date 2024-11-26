ca2cac6f0eb4822ac074daec5f552c49ed76dba7
onnik <onni.kivinen@gmail.com>
2024-11-26 14:52:43 +0200

    shaib

--- hud.py
+++ hud.py
@@ -3,13 +3,10 @@ from fifo import Fifo
 from ssd1306 import SSD1306_I2C
 import time
 
-
 oled_width = 128
 oled_height = 64
 i2c = I2C(1, scl=Pin(15), sda=Pin(14), freq=400000)
 oled_screen = SSD1306_I2C(oled_width, oled_height, i2c)
-
-
 
 old_time = 0
 
@@ -27,7 +24,6 @@ class Encoder:
         else:
             self.fifo.put(1)
 
-
 rot = Encoder(10, 11)
 
 # Nappulan luokka, lisää arvon 2 fifoon jos nappia painetaan.
@@ -36,7 +32,6 @@ class Button:
         self.button = Pin(button, mode=Pin.IN, pull=Pin.PULL_UP)
         self.button.irq(handler=self.button_handler, trigger=Pin.IRQ_FALLING, hard=True)
         self.fifo = Fifo(30, typecode="i")
-
 
     def button_handler(self, pin):
         delay = 200
@@ -48,9 +43,7 @@ class Button:
 
 button = Button(12)
 
-
 class Display:
-    
     def __init__(self):
         oled_screen.fill(0)
         self.menu_items = ["HR", "HRV", "HISTORY", "KUBIOS"]
@@ -83,10 +76,10 @@ class Display:
             oled_screen.text(f"{pointer} {list_item}", 0, i * 10, 1)
         
         oled_screen.show()
+
 display = Display()
 
-
-
-while True:
+#TESTAUS
+while True: 
     while rot.fifo.has_data():
         display.state()
