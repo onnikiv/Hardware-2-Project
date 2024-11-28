22a4df163cf0555acf5d48c8b6f9717508ab6ae2
JarkkoKarki <karkijarkko@gmail.com>
2024-11-28 11:21:30 +0200

    Kubios working

--- kubios.py
+++ kubios.py
@@ -1,6 +1,7 @@
 import network
 from time import sleep
 from umqtt.simple import MQTTClient
+import ujson
 
 # Replace these values with your own
 SSID = "KME759_Group_2"
@@ -13,7 +14,6 @@ def connect_wlan():
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     wlan.connect(SSID, PASSWORD)
-
     # Attempt to connect once per second
     while wlan.isconnected() == False:
         print("Connecting... ")
@@ -27,27 +27,39 @@ def connect_mqtt():
     mqtt_client.connect(clean_session=True)
     return mqtt_client
 
+def message_callback(topic, msg):
+    try:
+        message = ujson.loads(msg)
+        print(message)
+        print(f"Stress index: {message["data"]["analysis"]["stress_index"]}")
+    except Exception as e:
+        print("failed delivering message", e)
+
 # Main program
 if __name__ == "__main__":
-    #Connect to WLAN
-    connect_wlan()
-    
-    # Connect to MQTT
     try:
+        connect_wlan()
         mqtt_client=connect_mqtt()
-        
-    except Exception as e:
-        print(f"Failed to connect to MQTT: {e}")
+        mqtt_client.set_callback(message_callback)
+        mqtt_client.subscribe("hr-data") 
+        mqtt_client.subscribe("kubios-response")
 
-    # Send MQTT message
-    try:
         while True:
             # Sending a message every 5 seconds.
             topic = "kubios-request"
-            message = "Great job group X!"
-            mqtt_client.publish(topic, message)
-            print(f"Sending to MQTT: {topic} -> {message}")
+            message = {
+                "id": 123,
+                "type":"RRI",
+                "data": [828, 836, 852, 760, 800, 796, 856, 824, 808, 776, 724, 816, 800, 812, 812,
+812, 756, 820, 812, 800],
+                "analysis": {"type": "readiness" }
+                    }
+            msg = ujson.dumps(message)
+            mqtt_client.publish(topic, msg)
             sleep(5)
+            mqtt_client.check_msg()
+
             
     except Exception as e:
         print(f"Failed to send MQTT message: {e}")
+
