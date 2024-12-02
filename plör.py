e1dc1c04ccb8f8f0b166b91f6b6407fe9d90b3d4
onnik <onni.kivinen@gmail.com>
2024-12-02 19:05:02 +0200

    onnin lisää plöröö // Testausta

--- plör.py
+++ plör.py
@@ -2,7 +2,7 @@ from machine import Pin, I2C, ADC
 from fifo import Fifo
 from ssd1306 import SSD1306_I2C
 import micropython
-import time
+import utime
 adc = ADC(26)
 micropython.alloc_emergency_exception_buf(200)
 
@@ -22,7 +22,7 @@ def HR():
         adc_value = adc.read_u16()
         
         if 20000 < adc_value < 50000:
-            scaled = (adc_value * oled_height // 65535)
+            scaled = round(adc_value * oled_height // 65535)
 
             oled_screen.pixel(int(y), int(oled_height - scaled), colour)
             oled_screen.show()
@@ -32,33 +32,73 @@ def HR():
                 oled_screen.fill(0)
 
 
-import utime
 
-class Syke:
-    def __init__(self):
-        self.MIN_THRESHOLD = 20000
-        self.MAX_THRESHOLD = 50000
-        self.peaks = []
-        self.peak_times = []
+MAX_THRESHOLD = 50000
+MIN_THRESHOLD = 20000
 
-    def get_adc(self):
-        return adc.read_u16()
+        
+
+def read_sample():
+    utime.sleep_ms(10)
+    return adc.read_u16()
+        
     
-    def read_adc(self):
-        max_peak = 0
 
-        while True:
-            adc_value = self.get_adc()
-            current_time = utime.ticks_ms()
-            print(current_time)
-            utime.sleep_ms(10)
+
+
+def keep_reading():
+    values = []
+    times = []
+    start_time = utime.ticks_ms()  # ekaa kertaa otetaan aika
+    first_peak_time = next_peak_time = None #Alustetaan tulevat huiput
+    
+    y=0
+    colour = 1
+    oled_screen.fill(0)
+    
+    while True:
+        
+        sample = read_sample()
+        current_time = utime.ticks_ms() # uusi nykyinen aika katsotaan
+        
+        
+        if MIN_THRESHOLD < sample < MAX_THRESHOLD: # Jos arvot on siellä 30 tuhannessa niin dibdib
+            time_since_start = utime.ticks_diff(current_time, start_time) #ticks diffil katotaan kuinka paljon aikaa on alusta verratuna kuinka kauan looppi on pyörinyt
+            # DEBUG print(f"Time since start: {time_since_start} ms")
             
+            scaled_adc_value = (sample * oled_height // 65535)
+            values.append(scaled_adc_value)
+            times.append(current_time)
+            
+    # Täs kohtaa piirretään näyttöön skaalatut arvot
+            oled_screen.pixel(int(y), int(oled_height - scaled_adc_value), colour)
+            oled_screen.show()
+            y += 1
+            if y >= oled_width:
+                y = 0
+                oled_screen.fill(0) #Päivitetään jos y akselilla ylitetään OLED:in maximi
+            
+            if len(values) >= 50: # Jos listan pituus ylittää 100 niin mennään takasin alkuun
+                min_value = min(values)
+                max_value = max(values)
+                max_index = values.index(max_value)
+                max_time = times[max_index]
+                max_time_since_start = utime.ticks_diff(max_time, start_time)
+                
+                print(f"Min: {min_value}, Max: {max_value}")
+                print(f"Time of max value: {max_time_since_start} ms")
+                
+                if first_peak_time is None:
+                    first_peak_time = max_time
+                else:
+                    next_peak_time = max_time
+                    interval = next_peak_time - first_peak_time
+                    print(f"Interval between peaks: {interval} ms")
+                    first_peak_time = next_peak_time  # Update first_peak_time for the next interval calculation
+                
+                values = []
+                times = []
+                threshold = (min_value + max_value) // 2
+                print(f"Threshold: {threshold}")
 
-
-    def execute(self):
-        self.read_adc()
-
-syke = Syke()
-
-while True:
-    syke.execute()
+keep_reading()
