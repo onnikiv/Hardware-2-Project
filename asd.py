743968917e164063a958638e099d0db704e4b79e
onnik <onni.kivinen@gmail.com>
2024-12-03 18:53:43 +0200

    tweaks

--- asd.py
+++ asd.py
@@ -12,31 +12,26 @@ micropython.alloc_emergency_exception_buf(200)
 MAX_THRESHOLD = 50000
 MIN_THRESHOLD = 20000
 
-sample_rate = 100  # 250 Hz näytteenottotaajuus
+sample_rate = 100 # 10ms välein lisätään arvo fifoon
 
 
 
 
 class isr_adc:
     def __init__(self, adc_pin_nr):
-        self.av = ADC(adc_pin_nr)  # Alustaa ADC:n tietylle pinnille
-        self.samples = Fifo(50)  # FIFO-puskuri, johon arvot tallennetaan
-        self.dbg = Pin(0, Pin.OUT)  # Debug-pinni
+        self.av = ADC(adc_pin_nr)
+        self.samples = Fifo(50)
+        self.dbg = Pin(0, Pin.OUT)
 
     def handler(self, tid):
-
-        adc_value = self.av.read_u16()  # Lukee ADC-arvon
+        adc_value = self.av.read_u16()
         
-        # Tallenna ADC-arvo ja siihen kulunut aika FIFO-puskurille
         self.samples.put(self.av.read_u16())
-    
         # Debug-pinni
         self.dbg.toggle()
         
-# Luodaan isr_adc-instanssi
-ia = isr_adc(26)
 
-# Aikavälin ajastin
+ia = isr_adc(26)
 tmr = Piotimer(period=10, mode=Piotimer.PERIODIC, callback=ia.handler)
 
 
@@ -48,9 +43,7 @@ previous_peak_index = 0
 current_peak_index = 0
 average = 0
 
-# Pääsilmukka
 while True:
-
     if not ia.samples.empty():
         value = ia.samples.get()
         
@@ -60,7 +53,7 @@ while True:
                 previous_peak_index = current_peak_index
                 current_peak_index = reading_count
                 if previous_peak_index != 0:
-                    ppi = (current_peak_index - previous_peak_index)  * 10# 10 ms väli
+                    ppi = (current_peak_index - previous_peak_index)  * 10
                     hr = int(60 / (ppi / 1000))
 
                     if 30 <= hr <= 200:
