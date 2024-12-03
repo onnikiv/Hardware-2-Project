7aba355d186dd47995293065e95d3f5a72c66516
onnik <onni.kivinen@gmail.com>
2024-12-03 19:16:42 +0200

    uudelleennimeäminen

--- uusinsyke.py
+++ uusinsyke.py
@@ -9,13 +9,14 @@ import time
 
 micropython.alloc_emergency_exception_buf(200)
 
-MAX_THRESHOLD = 50000
-MIN_THRESHOLD = 20000
+oled_width = 128
+oled_height = 64
+i2c = I2C(1, scl=Pin(15), sda=Pin(14), freq=400000)
+oled_screen = SSD1306_I2C(oled_width, oled_height, i2c)
+
+
 
 sample_rate = 100 # 10ms välein lisätään arvo fifoon
-
-
-
 
 class isr_adc:
     def __init__(self, adc_pin_nr):
@@ -29,12 +30,13 @@ class isr_adc:
         self.samples.put(self.av.read_u16())
         # Debug-pinni
         self.dbg.toggle()
-        
+
 
 ia = isr_adc(26)
-tmr = Piotimer(period=10, mode=Piotimer.PERIODIC, callback=ia.handler)
+tmr = Piotimer(mode=Piotimer.PERIODIC, freq=sample_rate, callback=ia.handler)
 
-
+MAX_THRESHOLD = 50000
+MIN_THRESHOLD = 20000
 max_peak = 0
 reading_count = 0
 READING_LIMIT = 100
@@ -68,3 +70,4 @@ while True:
         if reading_count >= READING_LIMIT:
             max_peak = 0
             reading_count = 0
+    
